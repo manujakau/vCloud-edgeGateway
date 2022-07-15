62e3e8e6a97e22723da883f674dc373578907358
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-07-15 11:08:31 +0300

    add variables for routed networks

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,9 +1,23 @@
+vCloudAppliance = ""
+vCloudUser         = ""
+vCloudUserPassword = ""
+
 Organization       = ""
 VirtualDataCenter  = ""
 
 Tier0Gateways      = ""
 Tier0ExternalIP    = ""
 
-vCloudAppliance = ""
-vCloudUser         = ""
-vCloudUserPassword = ""
+EdgeGateway_gateway_CIDR = ""
+EdgeGateway_gateway_CIDR_prefix = ""
+
+VDCnetworkName = ""
+VDCnetworkGateway = ""
+VDCnetworkGateway_prefix = ""
+VDCnetworkGatewayDNS01 = ""
+VDCnetworkGatewayDNS02 = ""
+VDCnetworkGatewayStaticIPpool_start = ""
+VDCnetworkGatewayStaticIPpool_end = ""
+
+InternalIPsetCIDR = ""
+EXternalIpsetCIDR = ""
--- variables.tf
+++ variables.tf
@@ -51,3 +51,69 @@ variable "EdgeGatewayDescription" {
   description = "New EdgeGateway description"
   default     = ""
 }
+
+variable "EdgeGateway_gateway_CIDR" {
+  type        = string
+  description = "Tier-0-Gateway Gateway"
+  default     = ""
+}
+
+variable "EdgeGateway_gateway_CIDR_prefix" {
+  type        = string
+  description = "Tier-0-Gateway Gateway prefix"
+  default     = ""
+}
+
+variable "VDCnetworkName" {
+  type        = string
+  description = "New VDC network name backed by NSX-T"
+  default     = ""
+}
+
+variable "VDCnetworkGateway" {
+  type        = string
+  description = "New VDC network Gateway backed by NSX-T"
+  default     = ""
+}
+
+variable "VDCnetworkGateway_prefix" {
+  type        = string
+  description = "New VDC network Gateway prefix backed by NSX-T"
+  default     = ""
+}
+
+variable "VDCnetworkGatewayDNS01" {
+  type        = string
+  description = "New VDC network DNS01 backed by NSX-T"
+  default     = ""
+}
+
+variable "VDCnetworkGatewayDNS02" {
+  type        = string
+  description = "New VDC network DNS02 backed by NSX-T"
+  default     = ""
+}
+
+variable "VDCnetworkGatewayStaticIPpool_start" {
+  type        = string
+  description = "New VDC network static ip pool start address"
+  default     = "" 
+}
+
+variable "VDCnetworkGatewayStaticIPpool_end" {
+  type        = string
+  description = "New VDC network static ip pool end address"
+  default     = ""
+}
+
+variable "InternalIPsetCIDR" {
+  type        = string
+  description = "New VDC network internal IP set with prefix"
+  default     = ""
+}
+
+variable "EXternalIpsetCIDR" {
+  type        = string
+  description = "New VDC network external IP set with prefix"
+  default     = ""
+}
