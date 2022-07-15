04c51f0df438ce00cd005f249516f3f110cfffc5
manuja.karunathilaka <manuja.karunathilaka@telia.ee>
2022-07-15 10:36:21 +0300

    add variables

---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "vCloudUser" {
+  type        = string
+  description = "vCloud User name"
+  default     = ""
+}
+
+variable "vCloudUserPassword" {
+  type        = string
+  description = "vCloud User Password"
+  default     = ""
+}
+
+variable "Organization" {
+  type        = string
+  description = "New organization name"
+  default     = ""
+}
+
+variable "VirtualDataCenter" {
+  type        = string
+  description = "New VirtualDataCenter name"
+  default     = ""
+}
+
+variable "Tier0Gateways" {
+  type        = string
+  description = "Allocataed Tier0Gateways name"
+  default     = ""
+}
+
+variable "Tier0ExternalIP" {
+  type        = string
+  description = "Allocataed External IP from Tier0Gateway"
+  default     = ""
+}
+
+variable "EdgeGatewayName" {
+  type        = string
+  description = "New EdgeGateway name"
+  default     = ""
+}
+
+variable "EdgeGatewayDescription" {
+  type        = string
+  description = "New EdgeGateway description"
+  default     = ""
+}
